4028c2c3e8bf658a18d45a3cdfcfe6c0aec71aa4
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:12:57 +0600

    create services section in home page

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,30 +1,30 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// const ServicesSection = () => {
-//     const [data,setdata]=useState([])
-//     useEffect(()=>{
-//         fetch('http://localhost:5000/datas')
-//         .then(res=>res.json())
-//         .then(data=>setdata(data))
-//     },[])
-//     return (
-//         <div className="w-full border border-2 mb-10">
-//            <h1 className="text-center text-4xl font-bold py-5">Our Services</h1>
-//            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-24 gap-5 p-10 ">
-//             {
-//                 data.slice(0,6).map(data=><div key={data._id} className="flex border border-2 rounded-full space-x-5 items-center">
+const ServicesSection = () => {
+    const [data,setdata]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/datas')
+        .then(res=>res.json())
+        .then(data=>setdata(data))
+    },[])
+    return (
+        <div className="w-full border border-2 mb-10">
+           <h1 className="text-center text-4xl font-bold py-5">Our Services</h1>
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-24 gap-5 p-10 ">
+            {
+                data.slice(0,6).map(data=><div key={data._id} className="flex border border-2 rounded-full space-x-5 items-center">
                 
-//                 <div className="avatar">
-//                   <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-//                    <img src={data.image} />
-//                   </div>
-//                 </div>
-//                 <h1 className="px-5">{data.category}</h1>
-//             </div>)
-//             }
-//            </div>    
-//         </div>
-//     );
-// };
+                <div className="avatar">
+                  <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                   <img src={data.image} />
+                  </div>
+                </div>
+                <h1 className="px-5">{data.category}</h1>
+            </div>)
+            }
+           </div>    
+        </div>
+    );
+};
 
-// export default ServicesSection;
+export default ServicesSection;
