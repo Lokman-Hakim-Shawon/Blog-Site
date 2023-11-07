c7a00076f303cc8feaba11119e796c0113642d82
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:17:16 +0600

    complete private Route

--- src/authentication/PrivateRout.jsx
+++ src/authentication/PrivateRout.jsx
@@ -1,20 +1,20 @@
-// import { useContext } from "react";
-// import {Navigate} from "react-router-dom";
-// import { AuthContext } from "./Authprovider";
-// import { PropTypes } from 'prop-types';
+import { useContext } from "react";
+import {Navigate} from "react-router-dom";
+import { AuthContext } from "./Authprovider";
+import { PropTypes } from 'prop-types';
 
-// const PrivateRout = ({children}) => {
-//     const {user,loading}=useContext(AuthContext)
-//     if(loading){
-//         return <div className="flex justify-center"><span className="loading loading-spinner loading-lg"></span>
-//         </div>
-//     }
-//     if(user){
-//       return  children
-//     }
-//     return <Navigate to='/login'></Navigate>
-// };
-// PrivateRout.propTypes={
-//     children:PropTypes.node
-// }
-// export default PrivateRout;
+const PrivateRout = ({children}) => {
+    const {user,loading}=useContext(AuthContext)
+    if(loading){
+        return <div className="flex justify-center"><span className="loading loading-spinner loading-lg"></span>
+        </div>
+    }
+    if(user){
+      return  children
+    }
+    return <Navigate to='/login'></Navigate>
+};
+PrivateRout.propTypes={
+    children:PropTypes.node
+}
+export default PrivateRout;
