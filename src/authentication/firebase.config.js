85bc2f2b2e1cedb944e5517e5465684e2fc132c1
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:35:30 +0600

    create firebase.confige.js

--- src/authentication/firebase.config.js
+++ src/authentication/firebase.config.js
@@ -1,18 +1,18 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCxR6DXIYNolyK3jME70roCeRSpLWCI33g",
-//   authDomain: "decoded-petal-377806.firebaseapp.com",
-//   projectId: "decoded-petal-377806",
-//   storageBucket: "decoded-petal-377806.appspot.com",
-//   messagingSenderId: "924929920705",
-//   appId: "1:924929920705:web:2b379550769ea8eb584e40"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxR6DXIYNolyK3jME70roCeRSpLWCI33g",
+  authDomain: "decoded-petal-377806.firebaseapp.com",
+  projectId: "decoded-petal-377806",
+  storageBucket: "decoded-petal-377806.appspot.com",
+  messagingSenderId: "924929920705",
+  appId: "1:924929920705:web:2b379550769ea8eb584e40"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
