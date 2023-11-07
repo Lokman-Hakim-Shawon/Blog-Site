40d60ba6f4df5e5a34b026b2d656b658a8460133
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:36:19 +0600

    create AuthProvider

--- src/authentication/Authprovider.jsx
+++ src/authentication/Authprovider.jsx
@@ -1,55 +1,55 @@
-// import { createContext, useEffect, useState } from "react";
-// import app from "./firebase.config";
-// import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
-// import PropTypes from 'prop-types'; // ES6
+import { createContext, useEffect, useState } from "react";
+import app from "./firebase.config";
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
+import PropTypes from 'prop-types'; // ES6
 
-// export const AuthContext=createContext(null)
-// const auth=getAuth(app)
-// const provider=new GoogleAuthProvider()
-// const Authprovider = ({children}) => {
-//     const [user,setuser]=useState(null)
-//     const [loading,setloading]=useState(true)
-//     useEffect(()=>{
-//            const onchange=onAuthStateChanged(auth,currentuser=>{
-//                 // console.log('manage user successfull',currentuser)
-//                 setuser(currentuser)
-//                 setloading(false)
-//             })
-//         return ()=>{
-//             onchange()
-//         };
-//     },[])
-//     const createuser=(email,password)=>{
-//         setloading(true)
-//         return createUserWithEmailAndPassword(auth,email,password)
-//     }
-//     const google=()=>{
-//         setloading(true)
-//        return signInWithPopup(auth,provider)
-//     }
-//     const login=(email,password)=>{
-//         setloading(true)
-//         return signInWithEmailAndPassword(auth,email,password)
-//     }
-//     const logout=()=>{
-//         setloading(true)
-//         return signOut(auth)
-//     }
-//     const authInfo={
-//         user,
-//         loading,
-//         createuser,
-//         login,
-//         logout,
-//         google
-//     }
-//     return (
-//         <AuthContext.Provider value={authInfo}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
-// Authprovider.propTypes={
-//     children:PropTypes.node
-// }
-// export default Authprovider;
+export const AuthContext=createContext(null)
+const auth=getAuth(app)
+const provider=new GoogleAuthProvider()
+const Authprovider = ({children}) => {
+    const [user,setuser]=useState(null)
+    const [loading,setloading]=useState(true)
+    useEffect(()=>{
+           const onchange=onAuthStateChanged(auth,currentuser=>{
+                // console.log('manage user successfull',currentuser)
+                setuser(currentuser)
+                setloading(false)
+            })
+        return ()=>{
+            onchange()
+        };
+    },[])
+    const createuser=(email,password)=>{
+        setloading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const google=()=>{
+        setloading(true)
+       return signInWithPopup(auth,provider)
+    }
+    const login=(email,password)=>{
+        setloading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const logout=()=>{
+        setloading(true)
+        return signOut(auth)
+    }
+    const authInfo={
+        user,
+        loading,
+        createuser,
+        login,
+        logout,
+        google
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+Authprovider.propTypes={
+    children:PropTypes.node
+}
+export default Authprovider;
