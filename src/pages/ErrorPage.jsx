1792e41b3fa09238dcfaebff75d624b8f757ba7d
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 21:26:22 +0600

    create error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,14 @@
-// import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
-// const ErrorPage = () => {
-//     const error=useRouteError()
-//     console.log(error)
-//     return (
-//         <div className="flex flex-col justify-center items-center p-24 my-32">
-//             <h1 className="text-6xl font-bold">{error.status}</h1>
-//            <h1 className="text-6xl font-bold">PAGE NOT FOUND</h1> 
-//         </div>
-//     );
-// };
+const ErrorPage = () => {
+    const error=useRouteError()
+    console.log(error)
+    return (
+        <div className="flex flex-col justify-center items-center p-24 my-32">
+            <h1 className="text-6xl font-bold">{error.status}</h1>
+           <h1 className="text-6xl font-bold">PAGE NOT FOUND</h1> 
+        </div>
+    );
+};
 
-// export default ErrorPage;
+export default ErrorPage;
