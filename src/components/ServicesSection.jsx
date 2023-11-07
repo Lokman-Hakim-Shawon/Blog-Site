0ab372072bfd97d09da11d243cf6d9d3a63e8b4f
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-11-07 22:11:39 +0600

    update services section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -12,7 +12,7 @@ const ServicesSection = () => {
            <h1 className="text-center text-4xl font-bold py-5">Our Services</h1>
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-24 gap-5 p-10 ">
             {
-                data.slice(0,6).map(data=><div key={data._id} className="flex border border-2 rounded-full space-x-5 items-center">
+                data.slice(0,6).map(data=><div key={data._id} className="flex border border-2 border-blue-500 rounded-full space-x-5 items-center">
                 
                 <div className="avatar">
                   <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
